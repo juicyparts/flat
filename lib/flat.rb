bba051de130825408a9e0c2ce76b8b257e30fcda
Mel Riffe <mriffe@gmail.com>
2014-09-24 18:14:59 -0400

    Updates Exceptions documentation

--- lib/flat.rb
+++ lib/flat.rb
@@ -68,11 +68,11 @@ require 'flat/file'
 #
 # == Exceptions
 #
-# * FlatFileError - Generic error class and superclass of all other errors raised by Flat.
-# * LayoutConstructorError - The specified layout definition was not valid.
-# * RecordLengthError - Generic error having to do with line lengths not meeting expectations.
-# * ShortRecordError - The incoming line was shorter than expections defined.
-# * LongRecordError - The incoming line was longer than expections defined.
+# * +FlatFileError+ - Generic error class and superclass of all other errors raised by Flat.
+# * +LayoutConstructorError+ - The specified layout definition was not valid.
+# * +RecordLengthError+ - Generic error having to do with line lengths not meeting expectations.
+# * +ShortRecordError+ - The incoming line was shorter than expections defined.
+# * +LongRecordError+ - The incoming line was longer than expections defined.
 #
 module Flat
 
