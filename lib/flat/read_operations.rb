767ae94ffe36d046399ad5a6b8b86e8a829e1d81
Mel Riffe <mriffe@gmail.com>
2014-09-25 17:01:14 -0400

    Impelements ReadOperations each_record Instance Method

--- lib/flat/read_operations.rb
+++ lib/flat/read_operations.rb
@@ -8,7 +8,35 @@ module ReadOperations
   module ClassMethods #:nodoc:
   end # => module ClassMethods
 
-  module InstanceMethods #:nodoc:
+  ##
+  # = Instance Methods
+  #
+  # Defines behavior for instances of a subclass of Flat::File regarding the
+  # reading and parsing of the file contents, line by line.
+  #
+  module InstanceMethods
+
+    # Iterate through each record (each line of the data file). The passed
+    # block is passed a new Record representing the line.
+    #
+    #    s = SomeFile.new
+    #    s.each_record(open('/path/to/file')) do |r|
+    #      puts r.first_name
+    #    end
+    #
+    def each_record io, &block
+      io.each_line do |line|
+        line.chop!
+        next if line.length.zero?
+
+        unless (self.width - line.length).zero?
+          raise RecordLengthError, "length is #{line.length} but should be #{self.width}"
+        end
+
+        yield create_record(line, io.lineno), line
+      end
+    end
+
   end # => module InstanceMethods
 
   def self.included receiver #:nodoc:
