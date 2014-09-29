91686db0342c6754b980d13a5f541446a7baa085
Mel Riffe <mriffe@gmail.com>
2014-09-29 10:47:37 -0400

    Increments gem version to 0.1.6.pre

--- lib/flat/version.rb
+++ lib/flat/version.rb
@@ -1,3 +1,3 @@
 module Flat #:nodoc:
-  VERSION = "0.1.5"
+  VERSION = "0.1.6.pre"
 end
--- spec/lib/flat_spec.rb
+++ spec/lib/flat_spec.rb
@@ -5,7 +5,7 @@ describe Flat do
 
   describe 'Version' do
     it 'should verify current gem version' do
-      expect(Flat::VERSION).to eq('0.1.5')
+      expect(Flat::VERSION).to eq('0.1.6.pre')
     end
   end
 
