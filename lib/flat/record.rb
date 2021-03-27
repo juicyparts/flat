278cf45543a0d1d872ae7ef7a44e350ac1835ccb
antarr <antarr.byrd@gmail.com>
2021-03-26 23:36:53 -0500

    Add Attributes Hash

--- lib/flat/record.rb
+++ lib/flat/record.rb
@@ -94,6 +94,13 @@ module Record
         end
       end
     end
+
+    #
+    # Returns all attributes as a hash
+    #
+    def attributes_hash
+      @attributes.each_with_object({}) { |var, hash| hash[var.to_s.delete("@")] = self.instance_variable_get(var) }
+    end
   end # => class Definition
 
 end # => module Record
